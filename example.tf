6d17907c8614979a3714d2d62f849f6508a52a2a
marena23 <58753521+marena23@users.noreply.github.com>
2020-09-19 08:27:42 -0400

    Update example.tf

--- example.tf
+++ example.tf
@@ -7,17 +7,9 @@ resource "aws_instance" "example" {
   instance_type = "t2.micro"
   key_name      = "keyPairMAMint"
 
-  provisioner "local-exec" {
-    command = "echo ${aws_instance.example.public_ip} > /tmp/ip_address_MA.txt"
-  }
 }
 
 resource "aws_eip" "ip" {
     vpc = true
     instance = aws_instance.example.id
 }
-
-output "ip" {
-  value = aws_eip.ip.public_ip
-}
-
