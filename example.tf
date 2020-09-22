8359c9d913acf56a52a65b4b676b716ec14d102b
marena23 <58753521+marena23@users.noreply.github.com>
2020-09-21 20:09:45 -0400

    Update example.tf

--- example.tf
+++ example.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "ec2instance" {
   ami           = var.amis[var.region]
   instance_type = "t2.micro"
   key_name      = "keyPairMAMint"
